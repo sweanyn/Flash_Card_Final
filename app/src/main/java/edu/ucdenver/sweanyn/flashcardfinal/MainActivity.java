1ca555199198381741bf7e3f211465dea07e2df4
Nate <np.sweany@gmail.com>
2023-06-24 17:53:25 -0600

    Initial Commit

--- app/src/main/java/edu/ucdenver/sweanyn/flashcardfinal/MainActivity.java
+++ app/src/main/java/edu/ucdenver/sweanyn/flashcardfinal/MainActivity.java
@@ -3,6 +3,7 @@ package edu.ucdenver.sweanyn.flashcardfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    void onButtonClick(View view) {}
 }
