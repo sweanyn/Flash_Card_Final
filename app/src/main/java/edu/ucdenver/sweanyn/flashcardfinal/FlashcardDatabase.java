4af32df8b6c6e876300a5d3337a6be5c73df8203
sejalbhatia <116205077+sejalbhatia@users.noreply.github.com>
2023-07-17 16:35:29 -0600

    Created dialog_add_book.xml

--- app/src/main/java/edu/ucdenver/sweanyn/flashcardfinal/FlashcardDatabase.java
+++ app/src/main/java/edu/ucdenver/sweanyn/flashcardfinal/FlashcardDatabase.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+//import androidx.room.vo.Database;
 
 @Database(entities = {Flashcard.class, Book.class}, version = 1)
 public abstract class FlashcardDatabase extends RoomDatabase {
